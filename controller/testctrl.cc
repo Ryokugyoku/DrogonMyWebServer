fd9c4a6369870c81d166c7d22d5c6cd739898661
Ryokugyoku <ryoku@ryokugyoku.tech>
2024-02-25 09:15:02 +0900

    firstCommit

---
+++ controller/testctrl.cc
@@ -0,0 +1,12 @@
+#include "testctrl.h"
+
+void testctrl::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    // write your application logic here
+    auto resp = HttpResponse::newHttpResponse();  // 新しいレスポンスインスタンスを生成
+    resp->setStatusCode(k200OK);  // HTTPステータスコード 200に設定
+    resp->setContentTypeCode(CT_APPLICATION_JSON);  // Header: Content typeをHTMLにする
+    resp->setBody("<h1>Hello World!</h1>");  // Body:
+
+    callback(resp);
+}
